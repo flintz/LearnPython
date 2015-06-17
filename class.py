2c1955ca34453ef2842a3493b02f83fe9364c4a7
flintz <beloslyudovdmitriy@gmail.com>
2015-06-17 09:38:48 +0200

    Update class.py

--- class.py
+++ class.py
@@ -14,3 +14,25 @@ p1 = phone(1)
 p.__class__
 p1.__class__
 
+class FirstClass():
+  def __init__(self, data):
+    self.data = "Hello!"
+                
+  def __str__(self):
+    return unicode(self.data)
+            
+  def setdata(self,data):
+    self.data = data
+                
+  def show(self):
+    print self.data
+
+
+class phone(object):
+    def __init__(self):
+        self.number = 10
+    def call(self):
+        print('call', self.number)
+p = phone()
+p1 = phone()
+p1.call()
