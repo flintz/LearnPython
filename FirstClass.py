a2a9521b1a6c5cf9b31a7c1cd8811109c7e29edc
flintz <beloslyudovdmitriy@gmail.com>
2015-06-17 08:55:29 +0200

    Update FirstClass.py

--- FirstClass.py
+++ FirstClass.py
@@ -1,1 +1,12 @@
-1
+class FirstClass():
+  def __init__(self, data):
+    self.data = "Hello!"
+                
+  def __str__(self):
+    return unicode(self.data)
+            
+  def setdata(self,data):
+    self.data = data
+                
+  def show(self):
+    print self.data
