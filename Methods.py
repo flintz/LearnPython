1c05b71ab02be188993fb91abf8a4243fc8b4a5e
flintz <beloslyudovdmitriy@gmail.com>
2015-06-17 09:00:40 +0200

    Update Methods.py

--- Methods.py
+++ Methods.py
@@ -1,1 +1,21 @@
-1
+def find():
+  str_1="This is test string"
+  index_position=str_1.find("test")
+  print(index_position)# return(number) or -1
+  return()
+    
+def index_try_except(): # Get position word in string, try: & except: 
+  str_1="This is test string"
+  try:
+    index_position=str_1.index("test")
+    print(index_position) # Return(number) or Error
+  except ValueError:
+    print("The substring was not found")
+  return()
+    
+def url_string():
+  url_string='www.google.com'
+  if url_string.endswith('.com')
+    print('.com is that')
+  else:
+    print('.com is not that')
